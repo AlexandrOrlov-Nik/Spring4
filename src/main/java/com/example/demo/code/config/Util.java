0a457b5c49833de06d4e12a000f01abba0799882
darya <eagle2020327@gmail.com>
2024-10-01 21:35:02 +0300

    DataBase

---
+++ src/main/java/com/example/demo/code/config/Util.java
@@ -0,0 +1,59 @@
+package com.example.demo.code.config;
+
+
+import com.example.demo.code.models.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@org.springframework.context.annotation.Configuration
+public class Util {
+
+
+    private static final String URL = "jdbc:mysql://localhost:3306/my_database";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "user_password";
+
+    public static Connection getConnection() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+
+    public static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.addAnnotatedClass(User.class);
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USERNAME);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            return configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed" + ex);
+        }
+
+    }
+
+}
--- src/main/java/com/example/demo/code/models/User.java
+++ src/main/java/com/example/demo/code/models/User.java
@@ -1,10 +1,18 @@
 package com.example.demo.code.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class User {
    private int id;
 
-
+   @NotEmpty(message = "Name should  not be empty")
+   @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
    private String name;
+
+   @NotEmpty(message = "Email should  not be empty")
+   @Email(message = "Email should be valid")
    private String email;
 
    public User(){
